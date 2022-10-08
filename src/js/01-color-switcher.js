5d7c9367aceaef6487caa5cfebb7df557d2660ed
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-10-09 00:30:54 +0300

    task1 colorswitcher restored

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+import { getRandomHexColor } from '../js/template function/01-getRandomHexColor';
 
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+idInterval = ``;
+
+function onStart() {
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(idInterval);
+}
